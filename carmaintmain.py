3ed2390f13d6a67ac1c6bd27a1c7f687d458f0a6
Jeffrey Dean <59969527+j-j-dean@users.noreply.github.com>
2021-01-22 12:45:25 -0500

    Update carmaintmain.py

--- carmaintmain.py
+++ carmaintmain.py
@@ -14,7 +14,6 @@ FUNCTION
                                       object, and initializes the widgets in the view
                                       allowing the user to monitor their car maintenance
     do_exit                        -- exit the application
-    clear_all_windows              -- clears all the visible views
     activate_add_car_window        -- activates AddCarFrame object view
     activate_items_window          -- activates ItemsFrame object view
     activate_add_items_window      -- activates AddItemsFrame object view
@@ -25,7 +24,7 @@ FUNCTION
 
 DATA
     my_gui                         -- contains the CarMaintenanceGUI object
-    self.tk                        -- cotains tkinter to allow creating window elements
+    self.tk                        -- contains tkinter to allow creating window elements
                                       (Frames, Labels, Buttons, Entries)
     self.car_data                  -- contains the CarMaintenance data object
 """
@@ -41,87 +40,62 @@ import performmaintframe as pf
 import maintinfoframe as mif
 from tkinter import *
 
-# provide data/methods for managing views
+
+#
+# Provide data/methods for managing views
+#
 class CarMaintenanceGui:
 
     # initialize the view contents and store data in object
     def __init__(self):
         self.tk = Tk()
-        self.tk.geometry("300x460")
+        self.tk.geometry("300x620")
         self.tk.title("Car Maintenance")
+        self.tk.background = "#ddd"
 
-        # Initialize the view for entering new car data
-        self.add_car_frame = acf.AddCarFrame(self, self.tk,)
-
-        # Initialize the view for displaying maintenance item information for a selected car
-        self.items_frame = itf.ItemsFrame(self, self.tk)
-
-        # Initialize the view for adding maintenance items for a selected car
-        self.add_items_frame = aif.AddItemsFrame(self, self.tk)
-
-        # Initialize the view for adding maintenance performed information for an item for a selected car
-        self.perform_maint_frame = pf.PerformMaintFrame(self, self.tk)
-
-        # Initialize the view for updating the mileage information for selected car
-        self.update_mileage_frame = umf.UpdateMileageFrame(self, self.tk)
-
-        # Initialize the view for displaying current maintenance information
-        self.maint_info_frame = mif.MaintInfoFrame(self, self.tk)
-
-        # Initialize the view for the main view - this is invoked last to be displayed first
-        self.main_frame = mf.MainFrame(self, self.tk)
-
-        self.tk.mainloop()
+        # Initialize the view for the main view
+        self.frame  = mf.MainFrame(self, self.tk).frame
 
     # exits the application saving any unsaved data
     def do_exit(self):
-        saved_car_data = cm.store_car_maintenance_data(cm.car_data, cm.storage_file_name)
+        cm.store_car_maintenance_data(cm.car_data, cm.storage_file_name)
         self.tk.quit()
-
-    # clears all views
-    def clear_all_windows(self):
-        self.add_car_frame.clear_add_car_window()
-        self.items_frame.clear_items_window()
-        self.add_items_frame.clear_add_items_window()
-        self.update_mileage_frame.clear_update_mileage_window()
-        self.perform_maint_frame.clear_perform_maint_window()
-        self.maint_info_frame.clear_maint_info_window()
-        self.main_frame.clear_main_window()
 
     # activates the AddCarFrame view
     def activate_add_car_window(self):
-        self.clear_all_windows()
-        self.add_car_frame.activate_add_car_window()
+        self.frame.destroy()
+        self.frame = acf.AddCarFrame(self,self.tk).frame
 
     # activates the ItemsFrame view
     def activate_items_window(self):
-        self.clear_all_windows()
-        self.items_frame.activate_items_window()
+        self.frame.destroy()
+        self.frame = itf.ItemsFrame(self, self.tk).frame
 
     # activates the AddItemsFrame view
-    def activate_add_items_window(self, update=False):
-        self.clear_all_windows()
-        self.add_items_frame.activate_add_items_window(update)
+    def activate_add_items_window(self):
+        self.frame.destroy()
+        self.frame = aif.AddItemsFrame(self, self.tk).frame
 
     # activates the UpdateMileageFrame view
     def activate_update_mileage_window(self):
-        self.clear_all_windows()
-        self.update_mileage_frame.activate_update_mileage_window()
+        self.frame.destroy()
+        self.frame = umf.UpdateMileageFrame(self, self.tk).frame
 
     # activates the PerformMaintFrame view
     def activate_perform_maint_window(self):
-        self.clear_all_windows()
-        self.perform_maint_frame.activate_perform_maint_window()
+        self.frame.destroy()
+        self.frame = pf.PerformMaintFrame(self, self.tk).frame
 
     # activate the MaintInfoFrame view
     def activate_maint_info_window(self):
-        self.clear_all_windows()
-        self.maint_info_frame.activate_maint_info_window()
+        self.frame.destroy()
+        self.frame = mif.MaintInfoFrame(self, self.tk).frame
 
     # activates the MainFrame view
     def activate_main_window(self):
-        self.clear_all_windows()
-        self.main_frame.activate_main_window()
+        self.frame.destroy()
+        self.frame = mf.MainFrame(self, self.tk).frame
+
 
 #
 # Main
@@ -131,6 +105,7 @@ class CarMaintenanceGui:
 # Activate and display the main window
 #
 my_gui = CarMaintenanceGui()
+my_gui.frame.mainloop()
 
 #
 # Register the exit handler
