ed8d5441ae1f94afac3b037e34ff471310c93ad4
Jeffrey Dean <59969527+j-j-dean@users.noreply.github.com>
2021-01-22 12:48:16 -0500

    Update maintinfoframe.py

--- maintinfoframe.py
+++ maintinfoframe.py
@@ -18,8 +18,7 @@ FUNCTION
                                      selected car in the maintenance list
     show_recent_maint             -- displays the recent maintenance for all items stored for all cars
     do_cancel                     -- returns control to the Main Frame object
-    clear_maint_info_window       -- calls method grid_forget() to remove the MaintInfoFrame window from view
-    activate_maint_info_window    -- sets the MaintInfoFrame object back into view
+    show_maint_needed             -- displays any needed maintenance for all items stored for all cars
 
 DATA
     self                  -- contains the Frame object used for viewing
@@ -31,40 +30,73 @@ DATA
 """
 
 import carmaintenance as cm
-import validation as v
 from tkinter import *
 import tkinter.scrolledtext as scrolledtext
+from PIL import ImageTk, Image
 
-#provide data/methods for the MaintInfo view
-class MaintInfoFrame:
+
+#
+# Provide data/methods for the MaintInfo view
+#
+class MaintInfoFrame(Frame):
 
     # initialize the window contents and store data in object
     def __init__(self, master, tk):
 
+        super(Frame).__init__()
         self.master = master
         self.tk = tk
 
-        self.maint_info_frame = Frame(self.tk)
-        self.maint_info_frame.grid(row=0, column=0, sticky=N+S+E+W)
+        self.frame = Frame(self.tk, bg=self.tk.background)
+        self.frame.pack(fill=BOTH, expand=TRUE)
+
+        # Create and insert phone background image
+        load_file = "phone-background.png"
+        img = Image.open(load_file)
+        photo = ImageTk.PhotoImage(img)
+        self.img_panel = Label(self.frame, image=photo, bg=self.tk.background)
+        self.img_panel.image = photo
+        self.img_panel.place(x=0, y=0, relwidth=1, relheight=1, anchor=NW)
+
+        load_file = "phone-hdr.png"
+        img = Image.open(load_file)
+        photo = ImageTk.PhotoImage(img)
+        self.img_hdr_panel = Label(self.frame, image=photo, bg=self.tk.background)
+        self.img_hdr_panel.image = photo
+        self.img_hdr_panel.place(x=20, y=80, anchor=NW)
 
         # Create cancel button to return to previous view
-        self.cancel_button = Button(self.maint_info_frame, text="X", command=self.do_cancel, font=("Helvetica", 16))
+        self.cancel_button = Button(self.frame, text="←", command=self.do_cancel,
+                                    font=("Helvetica", 16, "bold"), bg=self.tk.background)
         self.cancel_button.config(borderwidth=0)
-        self.cancel_button.grid(row=1, padx=5, pady=20, sticky=W)
+        self.cancel_button.place(x=10, y=95, anchor=NW)
 
         # Create maintenance information label
-        self.info_label = Label(self.maint_info_frame, text="Car Maintenance Info", font=("Helvetica", 16))
-        self.info_label.grid(row=1)
+        self.info_label = Label(self.frame, text="Car Maintenance Info",
+                                font=("Helvetica", 16), bg=self.tk.background)
+        self.info_label.place(x=155, y=150, anchor=CENTER)
+
+        # Create and insert wrench image
+        load_file = "wrench.png"
+        img = Image.open(load_file)
+        img = img.resize((30,30),Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(img)
+        self.img_panel = Label(self.frame, image=photo, bg=self.tk.background)
+        self.img_panel.image = photo
+        self.img_panel.place(x=35, y=150, anchor=CENTER)
 
         # Create scrollable text box to place maintenance info into
-        self.text_box = scrolledtext.ScrolledText(self.maint_info_frame, width=40, height=20, font=("Helvetica", 10))
-        self.text_box.grid(row=2)
+        self.text_box = scrolledtext.ScrolledText(self.frame, width=37, height=20, font=("Helvetica", 10))
+        self.text_box.place(x=9, y=170, anchor=NW)
 
         # Create show recent maintenance button to view recent maintenance performed
-        self.recent_maint_button = Button(self.maint_info_frame, text="Recent Maintenance Performed",
-                                          command=self.show_recent_maint, font=("Helvetica", 10))
+        self.recent_maint_button = Button(self.frame, text="Recent Maintenance Performed",
+                                          command=self.show_recent_maint,
+                                          font=("Helvetica", 10), bg=self.tk.background)
         self.recent_maint_button.config(borderwidth=2, width=24)
-        self.recent_maint_button.grid(row=3, pady=10)
+        self.recent_maint_button.place(x=150, y=520, anchor=CENTER)
+
+        self.show_maint_needed()
 
     # display all recent maintenance performed
     def show_recent_maint(self):
@@ -79,25 +111,19 @@ class MaintInfoFrame:
                     last_miles = cm.car_data.get_item_last_mileage(car, item)
                     last_date = cm.car_data.get_item_last_date(car, item)
                     msg_text += ' '*5 + item + '\n'
-                    msg_text += ' '*10 +'Mileage: ' + str(last_miles) + '\n'
+                    msg_text += ' '*10 + 'Mileage: ' + str(last_miles) + '\n'
                     msg_text += ' '*10 + 'Date: ' + str(last_date) + '\n'
             msg_text += '\n'
 
         self.text_box.delete('1.0', END)
         self.text_box.insert(INSERT, msg_text)
 
-    # cancels the request by clearing the view and returning to the MainFrame view
+    # activates the MainFrame view
     def do_cancel(self):
-        self.clear_maint_info_window()
         self.master.activate_main_window()
 
-    # removes the MaintInfo object from view
-    def clear_maint_info_window(self):
-        self.maint_info_frame.grid_forget()
-
-    # activates the MaintInfo object view and restores it's contents
-    def activate_maint_info_window(self):
-        self.maint_info_frame.grid()
+    # display maintenance needed for each car in Car Maintenance list
+    def show_maint_needed(self):
         cars_need_maint = False
         self.text_box.delete('1.0', END)
 
