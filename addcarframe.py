9ca61f4a63d2a7f62bfb3e331235b0e76a6606e6
Jeffrey Dean <59969527+j-j-dean@users.noreply.github.com>
2021-01-22 12:41:02 -0500

    Update addcarframe.py

--- addcarframe.py
+++ addcarframe.py
@@ -17,9 +17,6 @@ FUNCTION
     do_add                   -- adds an additional car entry to the CarMaintenance object
                                 and returns control to the MainFrame object
     do_cancel                -- returns control to the MainFrame object
-    clear_add_car_window     -- calls method grid_forget() to remove the AddCarFrame
-                                object from view
-    activate_add_car_window  -- sets the AddCarFrame object back into view
 
 DATA
     self                  -- contains the Frame object used for viewing
@@ -33,8 +30,12 @@ DATA
 import carmaintenance as cm
 import validation as v
 from tkinter import *
+from PIL import ImageTk, Image
 
-#provide data/methods for the add car view
+
+#
+# Provide data/methods for the add car view
+#
 class AddCarFrame:
 
     # initialize the window contents and store data in object
@@ -43,39 +44,63 @@ class AddCarFrame:
         self.master = master
         self.tk = tk
 
-        self.add_car_frame = Frame(self.tk)
-        self.add_car_frame.grid(row=0, column=0, sticky=N+S+E+W)
+        self.frame = Frame(self.tk, bg=self.tk.background)
+        self.frame.pack(fill=BOTH, expand=TRUE)
+
+        # Create and insert phone background image
+        load_file = "phone-background.png"
+        img = Image.open(load_file)
+        photo = ImageTk.PhotoImage(img)
+        self.img_panel = Label(self.frame, image=photo, bg=self.tk.background)
+        self.img_panel.image = photo
+        self.img_panel.place(x=0, y=0, relwidth=1, relheight=1, anchor=NW)
+        load_file = "phone-hdr.png"
+        img = Image.open(load_file)
+        photo = ImageTk.PhotoImage(img)
+        self.img_hdr_panel = Label(self.frame, image=photo, bg=self.tk.background)
+        self.img_hdr_panel.image = photo
+        self.img_hdr_panel.place(x=20, y=80, anchor=NW)
 
         # Create cancel button to return to previous view
-        self.cancel_button = Button(self.add_car_frame, text="X", command=self.do_cancel, font=("Helvetica", 16))
+        self.cancel_button = Button(self.frame, text="←", command=self.do_cancel,
+                                    font=("Helvetica", 16), bg=self.tk.background)
         self.cancel_button.config(borderwidth=0)
-        self.cancel_button.grid(row=1, padx=5, pady=20, sticky=W)
+        self.cancel_button.place(x=10, y=95, anchor=NW)
 
         # Create label requesting the addition of a new car to the maintenance list
-        self.add_label = Label(self.add_car_frame, text="Add New Car",font=("Helvetica", 16))
-        self.add_label.grid(row=1)
+        self.add_label = Label(self.frame, text="Add New Car",font=("Helvetica", 16),bg=self.tk.background)
+        self.add_label.place(x=150, y=150, anchor=CENTER)
+
+        # Create and insert car image
+        load_file = "car.png"
+        img = Image.open(load_file)
+        img = img.resize((75,75),Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(img)
+        self.img_panel = Label(self.frame, image=photo, bg=self.tk.background)
+        self.img_panel.image = photo
+        self.img_panel.place(x=150, y=230, anchor=CENTER)
 
         # Create entry to add a car name to the maintenance list
-        self.car_name_label = Label(self.add_car_frame, text = "Enter Car Name: ", font=("Helvetica", 12))
-        self.car_name_label.grid(row=2, padx=5, pady=5)
-        self.car_name_label.config(width=30)
-        self.car_name_entry = Entry(self.add_car_frame, command=None)
-        self.car_name_entry.grid(row=3, pady=5)
+        self.car_name_label = Label(self.frame, text="Enter Car Name:",
+                                    font=("Helvetica", 12), bg=self.tk.background)
+        self.car_name_label.place(x=150, y=300, anchor=CENTER)
+        self.car_name_entry = Entry(self.frame, command=None)
+        self.car_name_entry.place(x=150, y=335, anchor=CENTER)
         
         # Create entry to add car's mileage to the maintenance list
-        self.mileage_label = Label(self.add_car_frame, text = "Enter Mileage: ", font=("Helvetica", 12))
-        self.mileage_label.grid(row=4, padx=5, pady=5)
-        self.mileage_entry=Entry(self.add_car_frame)
-        self.mileage_entry.grid(row=5)
+        self.mileage_label = Label(self.frame, text = "Enter Mileage: ", font=("Helvetica", 12), bg=self.tk.background)
+        self.mileage_label.place(x=150, y=360, anchor=CENTER)
+        self.mileage_entry=Entry(self.frame)
+        self.mileage_entry.place(x=150, y=395, anchor=CENTER)
 
         # Create label to display information to the user
-        self.info_label = Label(self.add_car_frame, text="", fg="red", font=("Helvetica", 12))
-        self.info_label.grid(row=6, padx=5, pady=10)
+        self.info_label = Label(self.frame, text="", fg="red", font=("Helvetica", 12), bg=self.tk.background)
+        self.info_label.place(x=150, y=430, anchor=CENTER)
 
         # create save button to perform the action to add the car to the maintenance list
-        self.add_car_button = Button(self.add_car_frame, text="Save", command=self.do_add, font=("Helvetica", 12))
-        self.add_car_button.config(borderwidth=2)
-        self.add_car_button.grid(row=7, pady=30)
+        self.add_car_button = Button(self.frame, text="Save", command=self.do_add,
+                                     font=("Helvetica", 12), bg=self.tk.background)
+        self.add_car_button.place(x=15, y=530, anchor=SW)
 
     # adds an additional car entry to the CarMaintenance object
     def do_add(self):
@@ -83,42 +108,25 @@ class AddCarFrame:
         # change blanks to underscores in car name
         car_name = self.car_name_entry.get().replace(' ', '_')
 
-        info_text = ""
-
         # Verify this car is not already in the list
         if not cm.car_data.is_new_car(car_name):
-            info_text = "Enter a new car name!"
+            self.info_label.config(text="Enter a new car name!")
         else:
             mileage = self.mileage_entry.get()
 
             # Verify mileage entered as a number
             if not v.is_valid_number(mileage):
-                info_text = "Enter mileage as a number!"
+                self.info_label.config(text="Enter mileage as a number!")
             else:
                 cm.car_data.add_car(car_name, mileage)
                 self.car_name_entry.delete(0, END)
                 self.mileage_entry.delete(0, END)
                 cm.selections.set_car_selected(car_name)
 
-                # clear the add car view and return to the main view
-                self.clear_add_car_window()
+                # return to the main view
                 self.master.activate_main_window()
 
-        # update information label
-        self.info_label.config(text = info_text)
-
-    # cancels the request by clearing the view and returning to the main view
+    # cancels the request by returning to the main view
     def do_cancel(self):
-        self.clear_add_car_window()
         self.master.activate_main_window()
-
-    # removes from view the add car view
-    def clear_add_car_window(self):
-        self.add_car_frame.grid_forget()
-
-    # restores the view of the add car view and restores it's contents
-    def activate_add_car_window(self):
-        self.add_car_frame.grid(column =0, row =0)
-        self.info_label.config(text="")
-        self.car_name_entry.focus_set()
 
