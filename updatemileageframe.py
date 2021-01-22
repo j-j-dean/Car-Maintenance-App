b53b40acdddafe2f0b6b7459252224619b11e094
Jeffrey Dean <59969527+j-j-dean@users.noreply.github.com>
2021-01-22 12:51:03 -0500

    Update updatemileageframe.py

--- updatemileageframe.py
+++ updatemileageframe.py
@@ -18,9 +18,6 @@ FUNCTION
     do_update                      -- update the mileage in cm.car_data object and return control
                                       to the ItemsFrame object
     do_cancel                      -- returns control to the ItemsFrame object
-    clear_update_mileage_window    -- calls method grid_forget() to remove the UpdateMileageFrame window
-                                      from view
-    activate_update_mileage_window -- sets the UpdateMileageFrame object back into view
 
 DATA
     self                  -- contains the Frame object used for viewing
@@ -34,8 +31,12 @@ DATA
 import carmaintenance as cm
 import validation as v
 from tkinter import *
+from PIL import ImageTk, Image
 
-# provide data/methods for the UpdateMileage view
+
+#
+# Provide data/methods for the UpdateMileage view
+#
 class UpdateMileageFrame:
 
     # initialize the window contents and store data in object
@@ -44,39 +45,68 @@ class UpdateMileageFrame:
         self.master = master
         self.tk = tk
 
-        self.update_mileage_frame = Frame(self.tk)
-        self.update_mileage_frame.grid(row =0, column=0, columnspan=4, sticky=N+S+E+W)
+        self.frame = Frame(self.tk, bg=self.tk.background)
+        self.frame.pack(fill=BOTH, expand=TRUE)
+
+        # Create and insert phone background image
+        load_file = "phone-background.png"
+        img = Image.open(load_file)
+        photo = ImageTk.PhotoImage(img)
+        self.img_panel = Label(self.frame, image=photo, bg=self.tk.background)
+        self.img_panel.image = photo
+        self.img_panel.place(x=0, y=0, relwidth=1, relheight=1, anchor=NW)
+        load_file = "phone-hdr.png"
+        img = Image.open(load_file)
+        photo = ImageTk.PhotoImage(img)
+        self.img_hdr_panel = Label(self.frame, image=photo, bg=self.tk.background)
+        self.img_hdr_panel.image = photo
+        self.img_hdr_panel.place(x=20, y=80, anchor=NW)
 
         # Create cancel button to return to previous frame
-        self.cancel_button = Button(self.update_mileage_frame, text="X", command=self.do_cancel, font=("Helvetica", 16))
-        self.cancel_button.config(height=2, borderwidth=0)
-        self.cancel_button.grid(row=1, padx=5, pady=5, sticky=W)
+        self.cancel_button = Button(self.frame, text="←", command=self.do_cancel,
+                                    font=("Helvetica", 16), bg=self.tk.background)
+        self.cancel_button.config(borderwidth=0)
+        self.cancel_button.place(x=10, y=95, anchor=NW)
+
+        # Create and insert car image
+        load_file = "car.png"
+        img = Image.open(load_file)
+        img = img.resize((75,75),Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(img)
+        self.img_panel = Label(self.frame, image=photo, bg=self.tk.background)
+        self.img_panel.image = photo
+        self.img_panel.place(x=150, y=190, anchor=CENTER)
 
         # Create label to display car name
-        self.car_name_label = Label(self.update_mileage_frame, text="", font=("Helvetica", 16))
-        self.car_name_label.grid(row=1, pady=5)
+        self.car_name_label = Label(self.frame, text="", font=("Helvetica", 16), bg=self.tk.background)
+        self.car_name_label.place(x=150, y=250, anchor=CENTER)
 
         # Create entry to add car's mileage to maintenance list button
-        self.mileage_label = Label(self.update_mileage_frame, text="Update Current Mileage: ", font=("Helvetica", 12))
-        self.mileage_label.grid(row=3, padx=10)
-        self.mileage_label.config(height=2, width=30)
-        self.mileage_entry=Entry(self.update_mileage_frame)
-        self.mileage_entry.grid(row=4)
+        self.update_mileage_label = Label(self.frame, text="Update Current Mileage: ",
+                                   font=("Helvetica", 12), bg=self.tk.background)
+        self.update_mileage_label.place(x=150, y=320, anchor=CENTER)
+        self.update_mileage_entry=Entry(self.frame)
+        self.update_mileage_entry.place(x=150, y=350, anchor=CENTER)
 
         # Create label to return information to the user
-        self.info_label = Label(self.update_mileage_frame, text="", fg="red", font=("Helvetica", 12))
-        self.info_label.grid(row=5, padx=10, pady=5)
-        self.info_label.config(height=2)
+        self.info_label = Label(self.frame, text="", fg="red",
+                                font=("Helvetica", 12), bg=self.tk.background)
+        self.info_label.place(x=150, y=420, anchor=CENTER)
 
         # Create update button to update and return to ItemsFrame window
-        self.update_button = Button(self.update_mileage_frame, text="Update", command=self.do_update,
-                                    font=("Helvetica", 10))
-        self.update_button.config(height=1, borderwidth=2)
-        self.update_button.grid(row=6, padx=10, pady=40, sticky=W)
+        self.update_button = Button(self.frame, text="Update Mileage", command=self.do_update,
+                                    font=("Helvetica", 10), bg=self.tk.background)
+        self.update_button.place(x=150, y=515, anchor=CENTER)
+
+        car = cm.selections.get_car_selected()
+        self.update_mileage_entry.insert(0, str(cm.car_data.get_mileage(car)))
+        car_text_entry = car.replace('_', ' ')
+        self.car_name_label.config(text=car_text_entry)
+        self.info_label.config(text="")
 
     # update the mileage in cm.car_data object and return control to ItemsFrame view
     def do_update(self):
-        mileage = self.mileage_entry.get()
+        mileage = self.update_mileage_entry.get()
 
         # validate mileage was entered as valid number
         if not v.is_valid_number(mileage):
@@ -84,24 +114,8 @@ class UpdateMileageFrame:
         else:
             car = cm.selections.get_car_selected()
             cm.car_data.set_mileage(car, mileage)
-            self.clear_update_mileage_window()
             self.master.activate_items_window()
 
-    # cancels the request by clearing the window and returning to the ItemsFrame view
+    # activates the ItemsFrame view
     def do_cancel(self):
-        self.clear_update_mileage_window()
         self.master.activate_items_window()
-
-    # removes from view the UpdateMileageFrame view
-    def clear_update_mileage_window(self):
-        self.mileage_entry.delete(0, END)
-        self.update_mileage_frame.grid_forget()
-
-    # restores the view of the UpdateMileageFrame view
-    def activate_update_mileage_window(self):
-        self.update_mileage_frame.grid(column =0, row =0)
-        car = cm.selections.get_car_selected()
-        self.mileage_entry.insert(0, str(cm.car_data.get_mileage(car)))
-        car_text_entry = car.replace('_', ' ')
-        self.car_name_label.config(text=car_text_entry)
-        self.info_label.config(text="")
